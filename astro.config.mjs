75efd0a7a0f38add7438d841226cba2ea0e65ce4
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-08-19 22:45:43 +0500

    feat: add react to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://nitrovim.github.io/',
   outDir: 'dist',
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [react()]
 });
